01221ab7f59e2ec6c12522646ef7c36da4e8e822
ihor-nakonechnyy <nakonechnyy.i@gmail.com>
2024-09-16 20:59:11 +0200

    added task_4

--- src/main/java/com/typeConversions/Task_2.java
+++ src/main/java/com/typeConversions/Task_2.java
@@ -6,11 +6,20 @@ import java.util.Scanner;
 public class Task_2 {
 
 
-    static void main(String[] args) {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter a two-digit natural number (n): ");
         int n = scanner.nextInt();
+
+        int digit1 = n / 10;   // First digit (tens place)
+        int digit12 = n % 10;  // Second digit (units place)
+
+        // Calculate the sum of the digits
+        int sum = digit1 + digit12;
+
+        // Display the result
+        System.out.println("The sum of the digits of " + n + " is: " + sum);
 
     }
 }
---
+++ src/main/java/com/typeConversions/Task_4.java
@@ -0,0 +1,31 @@
+package com.typeConversions;
+
+import java.util.Scanner;
+
+public class Task_4 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = 1001;
+        int b = 5565;
+        int c = 9865;
+
+        a = b + b;
+        b = c - a;
+        c = a + b + c;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+    }
+
+}
+
+
+
+
+
+
+
