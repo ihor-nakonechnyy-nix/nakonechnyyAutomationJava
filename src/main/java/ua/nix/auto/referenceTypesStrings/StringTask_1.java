ed3fe8ca19cb82d04f3ea86aac6f65a0a9cf77f1
ihor-nakonechnyy <nakonechnyy.i@gmail.com>
2024-09-27 09:11:54 +0200

    new package: Reference typesStrings; new class: Reference types. Strings;

--- src/main/java/ua/nix/auto/referenceTypesStrings/StringTask_1.java
+++ src/main/java/ua/nix/auto/referenceTypesStrings/StringTask_1.java
@@ -4,19 +4,29 @@ import java.util.Scanner;
 
 
 /*A string is given. Output the first, last and middle (if any) characters.
-Write a program that outputs the part of the string up to and including the first point encountered.
+
 Also provide for outputting the number of spaces.*/
 
 public class StringTask_1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        String str1 = "ihorNakonechnyy";
+        System.out.print("input an expression: ");
+        String exampleString = scanner.nextLine();
+        int length =exampleString.length();
+        System.out.println(length);
+        int middleIndex = length / 2;
+        System.out.println(middleIndex);
 
 
-        System.out.println("First character: "+ str1.charAt(0));
-        System.out.println("First character: "+ str1.charAt(6));
-        System.out.println("First character: "+ str1.charAt(14));
+        System.out.println("First character: "+ exampleString.charAt(0));
+        System.out.println("Middle character: "+ exampleString.charAt(8));
+        System.out.println("Last character: "+ exampleString.charAt(10));
+
+
+        int index = exampleString.indexOf(exampleString);
+        System.out.println("Index of ' '");
+
 
     }
 }
