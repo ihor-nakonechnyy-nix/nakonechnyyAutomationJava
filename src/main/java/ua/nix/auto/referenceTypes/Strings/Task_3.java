6cd94b12188b04e17fbc62406ff283c5d1a6c640
ihor-nakonechnyy <nakonechnyy.i@gmail.com>
2024-10-23 17:42:53 +0200

    task 1 and task 3 are done

--- src/main/java/ua/nix/auto/referenceTypes/Strings/Task_3.java
+++ src/main/java/ua/nix/auto/referenceTypes/Strings/Task_3.java
@@ -16,19 +16,34 @@ public class Task_3 {
         System.out.println("Enter sentence that contains one word that longest that others: ");
 
         String sentence = scan.nextLine();
-
         String[] sentenceSplit = sentence.split(" ");
-
         String longestWord = "";
 
-        for (String W : sentenceSplit) {
-            if (W.length() > longestWord.length());
-            longestWord = W;
+        for (String word : sentenceSplit) {
+            if (word.length() > longestWord.length()) {
+
+                longestWord = word;
+
+            }
+        }
+
+            System.out.println("longest word: " + longestWord);
+
 
         }
 
-        System.out.println("longest word: " + longestWord);
+//    public static void main2(String[] Args) {
+//
+//        String txt = "string of words separated by spaces is entered";
+//
+//        String[] split = txt.split(" ");
+//        String longestWord = "";
+//
+//        for (int i = 0; i < longestWord.length(); i++) {
+//            if(split.length > longestWord.length());
+//            System.out.println(longestWord);
+//
+//        }
 
 
     }
-}
