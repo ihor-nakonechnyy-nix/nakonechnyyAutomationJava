171d974e51ff6c19a922a0878aaae81fb6f59f05
ihor-nakonechnyy <nakonechnyy.i@gmail.com>
2024-11-10 15:54:35 +0100

    task 4 edited and done

--- src/main/java/ua/nix/auto/Arrays/Task_4.java
+++ src/main/java/ua/nix/auto/Arrays/Task_4.java
@@ -1,16 +1,24 @@
 package ua.nix.auto.Arrays;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Task_4 {
 
     public static void main(String[] args) {
 
-        int[] arra = {1000, 200, 500, 300, 900, 700, 100, 600, 400, 800};
-        Arrays.sort(arra);
+        int[] arra;
+        arra = new int[10];
+        Scanner scann = new Scanner(System.in);
+        System.out.println("Enter ten numbers: ");
+        for (int i = 0; i < arra.length; i++) {
+            arra[i] = scann.nextInt();
+
+        }
         System.out.println("Elements of array sorted in ascending order: ");
         for (int i = 0; i < arra.length; i++) {
             System.out.println(arra[i]);
+            Arrays.sort(arra);
         }
 
 
