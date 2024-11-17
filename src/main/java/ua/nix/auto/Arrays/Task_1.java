02e6615af24b819dda6d635bc92004aec7e0767b
ihor-nakonechnyy <nakonechnyy.i@gmail.com>
2024-11-17 17:55:11 +0100

    task 1 massive is done

--- src/main/java/ua/nix/auto/Arrays/Task_1.java
+++ src/main/java/ua/nix/auto/Arrays/Task_1.java
@@ -3,10 +3,9 @@ package ua.nix.auto.Arrays;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 
 
-public class task_1 {
+public class Task_1 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
