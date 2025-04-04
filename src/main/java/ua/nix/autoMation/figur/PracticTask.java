efd627191b7d55e21e0c4553b586c0d33e0cba42
ihor-nakonechnyy <nakonechnyy.i@gmail.com>
2025-04-04 20:01:41 +0200

    While_task_9 Tasks have been completed;

--- src/main/java/ua/nix/autoMation/figur/PracticTask.java
+++ src/main/java/ua/nix/autoMation/figur/PracticTask.java
@@ -5,12 +5,6 @@ public class PracticTask {
 
         Circle circle_1 = new Circle("myCircle", 100);
         Circle circle_2 = new Circle("myCircle", 200);
-
-
-//        System.out.println(circle_1.name);
-//        System.out.println(circle_2.radius);
-//        System.out.println(circle_1.getArea());
-
         Rectangle rectangle_1 = new Rectangle("myRectangke", 50, 100);
         Triangle triangle_1 = new Triangle("myTriangle", 7, 9);
         Trapezium trapezium_1 = new Trapezium("myTrapezium", 10,10, 15);
