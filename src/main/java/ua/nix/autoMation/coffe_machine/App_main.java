59376c9b26289522279d395affdd294af34e297b
ihor-nakonechnyy <nakonechnyy.i@gmail.com>
2025-04-11 17:47:33 +0200

    created class: App_main; created slass: coffeMachineClass; Created methods: ingridientsAmountPrint ingridientsAmountCalculation

---
+++ src/main/java/ua/nix/autoMation/coffe_machine/App_main.java
@@ -0,0 +1,18 @@
+package ua.nix.autoMation.coffe_machine;
+
+
+public class App_main {
+    public static void main(String[] args) {
+
+        coffeMachineClass coffeMachine = new coffeMachineClass(200, 50, 15);
+
+        coffeMachine.ingridientsAmountPrint();
+
+
+    }
+
+
+}
+
+
+
