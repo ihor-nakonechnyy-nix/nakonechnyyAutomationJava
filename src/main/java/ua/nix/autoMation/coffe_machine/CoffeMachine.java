8588130d69c8615b9dd0777be8c016b518fb11e6
stoding <119893346+stoding@users.noreply.github.com>
2025-04-13 17:07:27 +0200

    Revert "created class: App_main;"

--- src/main/java/ua/nix/autoMation/coffe_machine/CoffeMachine.java
+++ src/main/java/ua/nix/autoMation/coffe_machine/CoffeMachine.java
@@ -1,35 +1,31 @@
 package ua.nix.autoMation.coffe_machine;
 
 
-public class coffeMachineClass {
+public class CoffeMachine {
 
-    private int water;
-    private int milk;
-    private int coffee_beans;
+    public int water;
+    public int milk;
+    public int coffee_beans;
 
-    public coffeMachineClass(int water, int milk, int coffee_beans) {
+    public CoffeMachine(int water, int milk, int coffee_beans) {
         this.water = water;
         this.milk = milk;
         this.coffee_beans = coffee_beans;
 
 
     }
-    public int[] ingridientsAmountCalculation() {
+
+    public int[] getIngridAmount() {
+
         int water = this.water * 25;
         int milk = this.milk * 25;
         int coffee_beans = this.coffee_beans * 25;
 
-        return new int[]{water, milk, coffee_beans};
-    }
-
-    public int[] ingridientsAmountPrint() {
         System.out.println("Water: " + water + " ml");
         System.out.println("Milk: " + milk + " ml");
         System.out.println("coffee beans: " + coffee_beans + " g");
+
         return new int[]{water, milk, coffee_beans};
     }
-
-
 }
-
 
